8619809bb81f33a37d7bc567868892d7f895fec5
Programmering <teknisk+programmering-GDC2025-GR6@game.unf.dk>
2025-07-11 14:40:15 +0200

    Opgave 1 færdig

---
+++ Assets/Evilscript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Evilscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hej jeg er evil");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
