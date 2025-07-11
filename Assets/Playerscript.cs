855635f6156eec5fb377a7953301538c0f7c8b6d
Programmering <teknisk+programmering-GDC2025-GR6@game.unf.dk>
2025-07-11 14:29:15 +0200

    vi har lavet en opgave som der hedder "den første opgave"

---
+++ Assets/Playerscript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    //Among us
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Among us");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Who dis?");
+    }
+}
